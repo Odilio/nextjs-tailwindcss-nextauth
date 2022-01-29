ff008abd82e347a53f3e180cbda03095cd11e43a
odilio <odilionoronha@gmail.com>
2022-01-29 20:59:03 -0300

    intial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Underfy</h1>
+
+      <main>
+        <Sidebar />
+        {/*Center*/}
+
+      </main>
+       
+       <div>{/*Player*/}</div> 
+
+    </div>
+  )
+}
