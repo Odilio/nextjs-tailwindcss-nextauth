10945d400933bd56a58e14ecd4d97a8896586d46
odilio <odilionoronha@gmail.com>
2022-01-29 23:33:52 -0300

    adição de pagina central

--- components/Center.js
+++ components/Center.js
@@ -1,4 +1,5 @@
 import {
+   ChevronDownIcon,
    HeartIcon,
     HomeIcon, LibraryIcon, PlusCircleIcon, RssIcon, SearchIcon
 } from "@heroicons/react/outline";
@@ -8,9 +9,16 @@ function Center () {
 
    console.log("session  ", session)
     return ( 
-        <div className="flex flex-grow">
+        <div className="flex flex-grow ">
            hi
-
+            <header>
+               <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full
+               p-1 pr-2">
+                  <img className="rounded-full w-10 h-10" src={session?.user?.image}></img>
+                  <h2>{session?.user?.name}</h2>
+                  <ChevronDownIcon className="h-5 w-5"></ChevronDownIcon>
+               </div>
+            </header>
         </div>
      )
    
