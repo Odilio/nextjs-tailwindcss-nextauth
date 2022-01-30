ca59b9ed77db3b0622f4f1e7e06ce0397242bcec
odilio <odilionoronha@gmail.com>
2022-01-29 23:45:53 -0300

    correção em botão flutuante no centrer

--- components/Center.js
+++ components/Center.js
@@ -11,14 +11,20 @@ function Center () {
     return ( 
         <div className="flex flex-grow ">
            hi
-            <header>
-               <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full
+            <header className="absolute top-5 right-8">
+               <div className="flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full
                p-1 pr-2">
-                  <img className="rounded-full w-10 h-10" src={session?.user?.image}></img>
+                  <img className="rounded-full w-10 h-10" src={session?.user?.image}/>
                   <h2>{session?.user?.name}</h2>
-                  <ChevronDownIcon className="h-5 w-5"></ChevronDownIcon>
+                  <ChevronDownIcon className="h-5 w-5"/>
+                 
                </div>
+              
             </header>
+            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white padding-8 `}>
+               <img className="rounded-full" src={session?.user?.image}/>
+               hi
+               </section>
         </div>
      )
    
